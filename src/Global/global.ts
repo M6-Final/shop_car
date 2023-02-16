146cb689fc9dd2aa2c63b6410eb9680c6e9aa75f
jefersonokdemais <jefersonokdemais@hotmail.com>
2023-02-16 15:18:05 -0300

    feat: Paleta de cores global

--- src/Global/global.ts
+++ src/Global/global.ts
@@ -8,23 +8,64 @@ export default createGlobalStyle`
     outline:0;
     box-sizing: border-box;
   }
+
   body,html{
     width: 100vw;
     height: 100vh;
-    }
-
+  }
 
   body {
     color: var(--color-text);
     -webkit-font-smoothing: antialiased;
-
   }
+
   body, input, button, textarea {
     font-family: 'Poppins';
   }
+
   button {
     text-align: center;
     cursor: pointer;
+  }
+
+  :root{
+    --brand1: #4529E6; 
+    --brand1: #5126EA;
+    --brand1: #B0A6F0;
+    --brand1: #EDEAFD;
+
+    --grey0: #0B0D0D;
+    --grey1: #212529;
+    --grey2: #495057;
+    --grey3: #868E96;
+    --grey4: #ADB5BD;
+    --grey5: #CED4DA;
+    --grey6: #DEE2E6;
+    --grey7: #E9ECEF;
+    --grey8: #F1F3F5;
+    --grey9: #F6F6F6;
+    --grey10: #F6F6F6;
+    --whiteFixed: #FFFFFF;
+
+    --alert1: #CD2B31;
+    --alert2: #FDD8D8;
+    --alert3: #FFE5E5;
+    --sucess1: #18794E;
+    --sucess2: #CCEBD7;
+    --sucess3: #DDF3E4;
+
+    --random1: #E34D8C;
+    --random2: #C04277;
+    --random3: #7D2A4D;
+    --random4: #7000FF;
+    --random5: #6200E3;
+    --random6: #36007D;
+    --random7: #349974;
+    --random8: #2A7D5F;
+    --random9: #153D2E;
+    --random10: #6100FF;
+    --random11: #30007D;
+    --random12: #937BB9;
   }
 
   ::-webkit-scrollbar-track {
